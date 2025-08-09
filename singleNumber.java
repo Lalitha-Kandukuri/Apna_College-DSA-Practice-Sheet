b6789b0d7596da1e3b2ea20306bda118efe2c434
Lalitha <144223545+Lalitha-Kandukuri@users.noreply.github.com>
2025-08-09 14:35:45 +0530

    Add files via upload

---
+++ singleNumber.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class singleNumber {
+
+    public int single(int[] nums){
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num,0)+1);
+        }
+
+        for (int num : map.keySet()) {
+            if (map.get(num) == 1) {
+                return num;
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        singleNumber obj = new singleNumber();
+
+        int[] nums = {4,1,2,2,1};
+        
+        int res = obj.single(nums);
+        System.out.println(res);
+    }
+}
